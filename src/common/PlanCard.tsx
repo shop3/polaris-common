5cdc5909273f39b80384e1eb4743ae6b1a6a7dc8
Colin Obrien <worldbest.pro@outlook.com>
2023-03-23 16:26:18 +0900

    feat: replace deplicated components in plan card

--- src/common/PlanCard.tsx
+++ src/common/PlanCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Stack, Button, Icon, DisplayText, Card } from '@shopify/polaris';
+import { LegacyStack, Button, Text, LegacyCard } from '@shopify/polaris';
 
 type PlanCardProps = {
   name: string;
@@ -25,41 +25,41 @@ const PlanCard: React.FC<PlanCardProps> = ({
   trialDays,
 }) => {
   return (
-    <Card>
-      <Stack vertical={true} alignment="fill">
-        <Stack distribution="center">
-          <DisplayText size="large">{name}</DisplayText>
-        </Stack>
-        <Card.Section>
-          <Stack vertical={true} alignment="fill">
-            <Stack distribution="center">
-              <DisplayText size="extraLarge">
+    <LegacyCard>
+      <LegacyStack vertical={true} alignment="fill">
+        <LegacyStack distribution="center">
+          <Text variant="heading3xl" as="h2">{name}</Text>
+        </LegacyStack>
+        <LegacyCard.Section>
+          <LegacyStack vertical={true} alignment="fill">
+            <LegacyStack distribution="center">
+            <Text variant="heading4xl" as="h1">
                 {paymentsMode === 'recuringPrice' ? recurringPrice : oneTimePrice} {currencyCode}
-              </DisplayText>
-            </Stack>
-            <Card.Section>
-              <Stack alignment="center" distribution="center" spacing="baseTight">
-                <DisplayText size="small">
+              </Text>
+            </LegacyStack>
+            <LegacyCard.Section>
+              <LegacyStack alignment="center" distribution="center" spacing="baseTight">
+              <Text variant="headingXl" as="h4">
                   {paymentsMode === 'recuringPrice'
                     ? recurringInterval === 'EVERY_30_DAYS'
                       ? 'Every 30 days'
                       : 'Every year'
                     : 'One time purchase'}
-                </DisplayText>
-              </Stack>
-            </Card.Section>
+                </Text>
+              </LegacyStack>
+            </LegacyCard.Section>
             <Button fullWidth size="large" primary>
               Buy now
             </Button>
-            <Stack vertical={true}>
-              <h5>Free Enterprise Edition trial: {trialDays}</h5>
-              {usageCappedAmount > 0 ? <h5 style={{ paddingTop: -2 }}>Additional charges may apply</h5> : null}
-              <h5>{usageTerms}</h5>
-            </Stack>
-          </Stack>
-        </Card.Section>
-      </Stack>
-    </Card>
+            <LegacyStack vertical={true}>
+              <Text variant="headingMd" as="h5">Free Enterprise Edition trial: {trialDays}</Text>
+              {usageCappedAmount > 0 ? <Text variant="headingMd" as="h5">Additional charges may apply</Text> : null}
+              <Text variant="headingMd" as="h5">{usageTerms}</Text>
+            </LegacyStack>
+          </LegacyStack>
+        </LegacyCard.Section>
+      </LegacyStack>
+    </LegacyCard>
   );
 };
 
